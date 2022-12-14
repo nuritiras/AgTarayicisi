e75ddf76b32e88ef09cf0940dea51aac83fe2bf7
Nuri TIRAŞ <nuritiras@gmail.com>
2022-12-15 00:06:51 +0300

    Son hali

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import scapy.all as scapy
+import optparse
 
 #1) arp_request
 #2) broadcast
 #3) response
 
-arp_request_packet = scapy.ARP()
-#scapy.ls(scapy.ARP())
-broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-#scapy.ls(scapy.Ether())
-combined_packet = broadcast_packet/arp_request_packet
-result=scapy.srp(combined_packet, timeout=1)
-print(result)
+def get_user_input():
+    parse_object = optparse.OptionParser()
+    parse_object.add_option("-i", "--ipaddress", "ip_address", help="Enter Ip Address")
+
+    (user_input, arguments) = parse_object.parse_args()
+
+    if not user_input.ip_address:
+        print("Enter Ip Address")
+
+    return user_input
+
+def scan_my_network(ip):
+    arp_request_packet = scapy.ARP(pdst=ip)
+    #scapy.ls(scapy.ARP())
+    broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    #scapy.ls(scapy.Ether())
+    combined_packet = broadcast_packet/arp_request_packet
+    (answered_list, unanswered_list) = scapy.srp(combined_packet, timeout=1)
+    answered_list.summary()
+
+user_ip_address = get_user_input()
+scan_my_network(user_ip_address.ip_address)
