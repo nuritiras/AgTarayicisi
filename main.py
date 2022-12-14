978ea836b305e6b8b4af83b3edd73ffb5930c618
Nuri TIRAŞ <nuritiras@gmail.com>
2022-12-14 23:27:17 +0300

    Print..

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import scapy.all as scapy
 #3) response
 
 arp_request_packet = scapy.ARP()
-scapy.ls(scapy.ARP())
+#scapy.ls(scapy.ARP())
+broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+#scapy.ls(scapy.Ether())
+combined_packet = broadcast_packet/arp_request_packet
+result=scapy.srp(combined_packet, timeout=1)
+print(result)
